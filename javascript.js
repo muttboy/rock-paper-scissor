67c7d298abdc2a311dba8c6d45b7ba43104cd264
muttboy <randallpq@gmail.com>
2023-06-10 02:19:15 -0700

    Create function that initiates 5 rounds of rock paper scissor via for loop

--- javascript.js
+++ javascript.js
@@ -1,13 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="javascript.js"></script>
-    <title>Rock Paper Scissor</title>
-</head>
-<body>
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let choices = ["rock", "paper", "scissor"]
+    let winner
+
+    for (i = 0; i < 5; i++) {
+        let playerChoice = prompt("Enter: Rock, Paper, or Scissor").toLowerCase();
+        let random = Math.floor(Math.random() * choices.length);
+        let computerChoice = choices[random];
+        if (playerChoice == computerChoice) {
+            result = "It's a Tie!";
+        } else if (playerChoice == "rock" && computerChoice == "scissor" || 
+                   playerChoice == "scissor" && computerChoice == "paper" ||
+                   playerChoice == "paper" && computerChoice == "rock") {
+            result = "You Win!";
+            playerScore++;
     
-</body>
-</html>
+        } else if (playerChoice == "rock" && computerChoice == "paper" ||
+                   playerChoice == "scissor" && computerChoice == "rock" ||
+                   playerChoice == "paper" && computerChoice == "scissor") {
+            result = "You Lose!";
+            computerScore++;
+    
+        } else {
+            result = "Input Error";
+        }
+    
+    console.log("You chose " + playerChoice + " & computer chose " + computerChoice + ".");
+    console.log(result + " Your score: " + playerScore + ", Computer score: " + computerScore);
+    
+    }
+
+    if (playerScore > computerScore) {
+        winner = "You are the Winner!";
+    } else if (playerScore < computerScore) {
+        winner = "You have been defeated!";
+    } else {
+        winner = "Draw!"
+    }
+    console.log(winner);
+    return
+}
+
+game();
+
